ad1dff7764db78f8917d8e88c94b1f5e48d60eef
Akzhan <agosh.kz@mail.ru>
2020-05-18 01:34:36 +0600

    lesson 21

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]')
-  const closeBtn = document.querySelector('.modal__close');
-
-    console.log(modalBtn)
-
-  const switchModal = () => {
-      console.log(modal)
-    modal.classList.toggle('modal--visible');
-  }
-
-
-  modalBtn.forEach(btn => btn.addEventListener('click', switchModal));
-
-  //* Close button inside of modal handler
-  closeBtn.addEventListener('click', switchModal);
-
-  //* Click outside of modal handler 
-  window.addEventListener('click', (e)=> e.target == modal && switchModal());
-  //* Esc off modal handler 
-  window.addEventListener('keydown', (e)=> modal.classList.contains('modal--visible') && e.key === 'Escape' && switchModal());
+$(document).ready( function() {
+  let modal = $('.modal'),
+  modalBtn = $('[data-toggle=modal]'),
+  closeBtn = $('.modal__close');
+  modalBtn.on('click', function(){
+    modal.toggleClass('modal--visible')
+  })
+  closeBtn.on('click', function(){
+    modal.toggleClass('modal--visible')
+  })
 });
+
+
+// document.addEventListener('DOMContentLoaded', (e) => {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]')
+//   const closeBtn = document.querySelector('.modal__close');
+
+//     console.log(modalBtn)
+
+//   const switchModal = () => {
+//       console.log(modal)
+//     modal.classList.toggle('modal--visible');
+//   }
+
+
+//   modalBtn.forEach(btn => btn.addEventListener('click', switchModal));
+
+//   //* Close button inside of modal handler
+//   closeBtn.addEventListener('click', switchModal);
+
+//   //* Click outside of modal handler 
+//   window.addEventListener('click', (e)=> e.target == modal && switchModal());
+//   //* Esc off modal handler 
+//   window.addEventListener('keydown', (e)=> modal.classList.contains('modal--visible') && e.key === 'Escape' && switchModal());
+// });
